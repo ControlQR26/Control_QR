1235ac61f709555f1a950db9e1f2087d2171d2af
Jherson Orlando Rivera Leon <jherson.rivera@alcyopal.local>
2026-07-28 15:29:19 -0500

    chore: hacer dinamico el endpoint de API para deploy en Render

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,10 +15,11 @@ const nextConfig = {
     serverComponentsExternalPackages: ['bcryptjs']
   },
   async rewrites() {
+    const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
     return [
       {
         source: '/api/:path((?!auth).*)',
-        destination: 'http://localhost:5000/api/:path',
+        destination: `${backendUrl}/:path`,
       },
     ];
   }
